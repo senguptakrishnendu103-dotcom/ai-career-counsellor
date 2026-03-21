6e9c21e64a3a42ca3011f763ac3b948547556087
senguptakrishnendu103-dotcom <senguptakrishnendu103@gmail.com>
2026-03-21 15:01:48 +0530

    Update server to use environment variable for PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Explanation
 function generateExplanation(score) {
