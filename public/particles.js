dd3c1951f6d0feb15263344705604797ccb53a24
senguptakrishnendu103-dotcom <senguptakrishnendu103@gmail.com>
2026-03-25 05:50:01 +0530

    Update particles.js

--- public/particles.js
+++ public/particles.js
@@ -1,16 +1,18 @@
-const canvas=document.getElementById("bg");
-const ctx=canvas.getContext("2d");
+const canvas = document.getElementById("bg");
+const ctx = canvas.getContext("2d");
 
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-let particles=[];
+let particles = [];
 
-for(let i=0;i<60;i++){
+for(let i=0;i<80;i++){
  particles.push({
   x:Math.random()*canvas.width,
   y:Math.random()*canvas.height,
-  r:2
+  r:Math.random()*2,
+  dx:Math.random()-0.5,
+  dy:Math.random()-0.5
  });
 }
 
@@ -22,8 +24,15 @@ function draw(){
   ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
   ctx.fillStyle="#3b82f6";
   ctx.fill();
+
+  p.x+=p.dx;
+  p.y+=p.dy;
  });
 
+ requestAnimationFrame(draw);
+}
+
+draw();
  requestAnimationFrame(draw);
 }
 
